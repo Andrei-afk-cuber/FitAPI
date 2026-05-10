806598916831eeb420797ece36dc5bf0ba32686e
Andrei Lyakh <Edgar.digl@mail.ru>
2026-05-10 18:15:28 +0300

    Project was configured.

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+import dotenv
+from dataclasses import dataclass
+
+
+dotenv.load_dotenv()
+
+@dataclass
+class DatabaseConfig:
+    DB_NAME: str = os.getenv('DB_NAME')
+    DB_USER: str = os.getenv('DB_USER')
+    DB_PASSWORD: str = os.getenv('DB_PASSWORD')
+    DB_HOST: str = os.getenv('DB_HOST')
+    DB_PORT: str = os.getenv('DB_PORT')
