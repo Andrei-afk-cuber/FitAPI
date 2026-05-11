70648ed62739e652215d8b3f859bc096218d0c2c
Andrei Lyakh <Edgar.digl@mail.ru>
2026-05-11 11:24:03 +0300

    Created get csrf-token view

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UserCreateView, UserUpdateView, UserListView, UserProfileView
+from .views import UserCreateView, UserUpdateView, UserListView, UserProfileView, get_csrf
 
 urlpatterns = [
+    path('csrf-token/',get_csrf),
     path('', UserListView.as_view(), name='user-list'),
     path('<int:pk>/', UserProfileView.as_view(), name='user-profile'),
     path('register/', UserCreateView.as_view(), name='register'),
--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
+from django.middleware.csrf import get_token
+from rest_framework.decorators import api_view
 from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserCreateSerializer, UserUpdateSerializer, UserListSerializer, UserRetrieveSerializer
 
+
+# view for get CRSF
+@api_view(['GET'])
+def get_csrf(request):
+    token = get_token(request)
+    return Response(token)
 
 # get users list view
 class UserListView(ListAPIView):
